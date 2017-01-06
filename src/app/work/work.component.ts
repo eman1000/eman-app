a2b145cdd76c06d10782ad81d6757ce4a78f19be
Emmancipate Musemwa <emman@aleph-labs.com>
2017-01-06 11:31:40 +0800

    Styled work item and added data

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -14,14 +14,25 @@ export class WorkComponent implements OnInit {
   getData:string;
   postData:string;
 
-  myData = {
-  	name:"Proj 1",
-  	description:"Proj Descr",
-  	category:"Design",
-  	client:"Some Client",
-  	headerImage:"someImage",
-  	projectImages:"sdsssdsd"
-  }
+    workItems = [{
+        id:"1",
+        projectName:"Taxi Booking System",
+        client:"Smart Taxi",
+        description:"This was my final year school project. It is a taxi reservation system that allows users to book a taxi using any device. The system also allows the tracking of a taxi driver’s location in real time. ",
+        category:"DESIGN / APP DEV",
+        projectImage:"http://emmancipatemusemwa.com/img/smart2.jpg",
+        projectWorkImage:"http://emmancipatemusemwa.com/img/route_taxi.jpg",
+        className:"col-xs-7"
+    },{
+        id:"2",
+        projectName:"TOLUCA",
+        client:"Toluca Media",
+        description:"The website was designed for a media company locat... ",
+        category:"DESIGN / BRANDING",
+        projectImage:"http://emmancipatemusemwa.com/img/toluca-card2.jpg",
+        projectWorkImage:"http://emmancipatemusemwa.com/img/toluca_screens.jpg",
+        className:"col-xs-5"
+    }]
 
   onTestGet(){
   	this._httpService.getCurrentTime()
